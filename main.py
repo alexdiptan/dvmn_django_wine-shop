c3bc5f5457f5477424a61ea21bf5102ca125c878
aleksandrdiptan <diptan@huntflow.ru>
2022-11-30 21:25:53 +0300

    add get_year_ending func

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape(["html"]))
+template = env.get_template("template.html")
+
+current_year = datetime.today().year
+years_since_foundation = current_year-1920
+
+
+def get_year_ending(digit):
+    exception_digits = [i for i in range(11, 20)]
+    for exception_digit in exception_digits:
+        if str(digit).endswith(str(exception_digit)):
+            return 'лет'
+    if (digit > 1 or digit < 5) or str(digit).endswith('2') or str(digit).endswith('3') or str(digit).endswith('4'):
+        return 'года'
+    if digit == 1 or str(digit).endswith('1'):
+        return 'год'
+    else:
+        return 'лет'
+
+
+rendered_page = template.render(
+    foundation=years_since_foundation,
+    year_ending=get_year_ending(years_since_foundation)
+)
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
