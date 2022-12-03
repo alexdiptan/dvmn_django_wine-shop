49e1a6d6ac81cd698f91066acb82c8675beda389
aleksandrdiptan <diptan@huntflow.ru>
2022-12-03 17:03:00 +0300

    add wines from xlsx-file

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape(["html"]))
 template = env.get_template("template.html")
 
 current_year = datetime.today().year
-years_since_foundation = current_year-1920
+years_since_foundation = current_year - 1920
 
 
 def get_year_ending(digit):
     exception_digits = [i for i in range(11, 20)]
     for exception_digit in exception_digits:
         if str(digit).endswith(str(exception_digit)):
-            return 'лет'
-    if (digit > 1 or digit < 5) or str(digit).endswith('2') or str(digit).endswith('3') or str(digit).endswith('4'):
-        return 'года'
-    if digit == 1 or str(digit).endswith('1'):
-        return 'год'
+            return "лет"
+    if (
+            (digit > 1 or digit < 5)
+            or str(digit).endswith("2")
+            or str(digit).endswith("3")
+            or str(digit).endswith("4")
+    ):
+        return "года"
+    if digit == 1 or str(digit).endswith("1"):
+        return "год"
     else:
-        return 'лет'
+        return "лет"
 
+
+excel_data_df = pandas.read_excel(
+    "wine.xlsx",
+    sheet_name="Лист1",
+)
 
 rendered_page = template.render(
     foundation=years_since_foundation,
-    year_ending=get_year_ending(years_since_foundation)
+    year_ending=get_year_ending(years_since_foundation),
+    wines=excel_data_df.to_dict(orient='records'),
 )
 
 with open("index.html", "w", encoding="utf8") as file:
